7ae12aba70bc5f7de39b97c1995f274692ad2bea
Sebastian Ocampo <sebastian45roa@gmail.com>
2024-07-23 15:02:05 -0500

    se modifican las rutas

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Header from "./components/Header/Header";
 import store from "./utils/store";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainContainer from "./components/MainContainer/MainContainer";
-import Watchpage from "./components/ComicDetail/ComicDetail";
 import Login from "./components/Login/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Cookies from 'js-cookie';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import ComicDetail from "./components/ComicDetail/ComicDetail";
 
 const onLoginSuccess = () => {
   window.location.href = '/';
@@ -26,10 +26,9 @@ function App() {
             <Route element={<ProtectedRoute />}>
               <Route path="/" element={<Body />}>
                 <Route index element={<MainContainer />} />
-                {/* <Route path="info/:id" element={<Watchpage />} /> */}
+                <Route path="info/:id" element={<ComicDetail />} />
               </Route>
             </Route>
-            <Route path="info/:id" element={<Watchpage />} />
           </Routes>
         </div>
       </BrowserRouter>
