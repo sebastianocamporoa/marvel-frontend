113ed4b21dc7d3146c418a50684dbde35ded6dc9
Sebastian Ocampo <sebastian45roa@gmail.com>
2024-07-23 01:29:15 -0500

    se crea formulario de registro

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,6 +7,8 @@ const Login = ({ onLoginSuccess }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
+    const [isLogin, setIsLogin] = useState(false);
+    const [identification, setIdentification] = useState('');
 
     const handleLogin = async (event) => {
         event.preventDefault();
@@ -23,37 +25,108 @@ const Login = ({ onLoginSuccess }) => {
         }
     };
 
+    const handleRegister = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post('https://marvel-backend-production-ff25.up.railway.app/users/register', {
+                email,
+                identification,
+                password,
+            });
+            const { accessToken } = response.data;
+            Cookies.set('accessToken', accessToken);
+            onLoginSuccess();
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
     const toggleShowPassword = () => {
         setShowPassword(!showPassword);
     };
 
+    const toggleForm = () => {
+        setIsLogin(!isLogin);
+    };
+
     return (
         <div className="login-container">
-            <form className='form' onSubmit={handleLogin}>
-                <img src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg' alt='logo' />
-                <h1>Login</h1>
-                <input
-                    type="email"
-                    placeholder='email'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <div className='form-group'>
+            {isLogin ? (
+                <form className='form' onSubmit={handleLogin}>
+                    <img src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg' alt='logo' />
+                    <h1 className='text-2xl font-bold'>Inicio de sesión</h1>
                     <input
-                        id='password'
-                        type={showPassword ? "text" : "password"}
-                        placeholder='Contraseña'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        type="email"
+                        placeholder='email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         required
                     />
-                    <button className='btn-show' type='button' onClick={toggleShowPassword}>
-                        <i id='passIcon' className={showPassword ? 'bi bi-eye-slash' : 'bi bi-eye'}></i>
-                    </button>
-                </div>
-                <button type="submit" className='btn-primary'>Iniciar sesión</button>
-            </form>
+                    <div className='form-group'>
+                        <input
+                            id='password'
+                            type={showPassword ? "text" : "password"}
+                            placeholder='Contraseña'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <button className='btn-show' type='button' onClick={toggleShowPassword}>
+                            <i id='passIcon' className={showPassword ? 'bi bi-eye-slash' : 'bi bi-eye'}></i>
+                        </button>
+                    </div>
+                    <button type="submit" className='btn-primary'>Iniciar sesión</button>
+                    <div className='toggle-form'>
+                        <p>
+                            {isLogin ? "¿No tienes una cuenta? " : "¿Ya tienes una cuenta? "}
+                            <span onClick={toggleForm} className='toggle-link'>
+                                {isLogin ? "Regístrate aquí" : "Inicia sesión aquí"}
+                            </span>
+                        </p>
+                    </div>
+                </form>
+            ) : (
+                <form className='form' onSubmit={handleRegister}>
+                    <img src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg' alt='logo' />
+                    <h1 className='text-2xl font-bold'>Registro</h1>
+                    <input
+                        type="text"
+                        placeholder='Identificación'
+                        value={identification}
+                        onChange={(e) => setIdentification(e.target.value)}
+                        required
+                    />
+                    <input
+                        type="email"
+                        placeholder='email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <div className='form-group'>
+                        <input
+                            id='password'
+                            type={showPassword ? "text" : "password"}
+                            placeholder='Contraseña'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <button className='btn-show' type='button' onClick={toggleShowPassword}>
+                            <i id='passIcon' className={showPassword ? 'bi bi-eye-slash' : 'bi bi-eye'}></i>
+                        </button>
+                    </div>
+                    <button type="submit" className='btn-primary'>Registrarse</button>
+                    <div className='toggle-form'>
+                        <p>
+                            {isLogin ? "¿No tienes una cuenta? " : "¿Ya tienes una cuenta? "}
+                            <span onClick={toggleForm} className='toggle-link'>
+                                {isLogin ? "Regístrate aquí" : "Inicia sesión aquí"}
+                            </span>
+                        </p>
+                    </div>
+                </form>
+            )}
         </div>
     );
 };
