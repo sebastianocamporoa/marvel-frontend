2905b8e30fcd8e16d376c484199cc15c9f8f3c07
Sebastian Ocampo <sebastian45roa@gmail.com>
2024-07-23 16:08:53 -0500

    se corrige ruta de redirección

--- src/components/FavoritesList/FavoritesList.js
+++ src/components/FavoritesList/FavoritesList.js
@@ -44,7 +44,7 @@ const FavoritesList = () => {
           <ShimmerCard />
         ) : (
           comics.map((comic) => (
-            <Link to={`/comic/${comic.comicId}`} key={comic.comicId}>
+            <Link to={"info/" + comic.comicId} key={comic.comicId}>
               <ComicCardFavorite key={comic.comicId} info={comic} />
             </Link>
           ))
