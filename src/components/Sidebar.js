cdca1013d6823a186f409568373cb1b9b74c6533
Sebastian Ocampo <sebastian45roa@gmail.com>
2024-07-22 22:40:13 -0500

    se crea opcion de cerrar sesión

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,32 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   AiOutlineHome,
 } from "react-icons/ai";
+import Cookies from 'js-cookie';
 
 const Sidebar = () => {
   const isToggleMenu = useSelector((state) => state.app.isMenuOpen);
-
-  // early return pattern
+  const navigate = useNavigate();
   if (!isToggleMenu) return null;
+  const handleLogout = () => {
+    Cookies.remove('accessToken'); // Elimina el token de autenticación
+    navigate('/login'); // Redirige al usuario a la página de inicio de sesión
+  };
   return (
     <div className="p-5 shadow-lg w-1/6 h-screen overflow-y-auto">
       <ul>
         <li className="py-2 flex hover:bg-gray-200 rounded-lg pl-2 font-bold">
           <Link to="/" className="flex">
             {" "}
-            <AiOutlineHome className="mr-5 mt-1 text-xl" /> Home
+            <AiOutlineHome className="mr-5 mt-1 text-xl" /> Inicio
           </Link>
+        </li>
+        <li className="py-2 flex hover:bg-gray-200 rounded-lg pl-2 font-bold">
+          <button onClick={handleLogout} className="flex w-full text-left">
+            <span className="mr-5 mt-1 text-xl">🔒</span> Cerrar Sesión
+          </button>
         </li>
       </ul>
     </div>
